99587b18ac7af4a3fe3b7576f9ac53ea53161174
Lakshya Sharma <lakshya4568@gmail.com>
2023-11-19 17:14:40 +0530

    Defined the swapPair function

--- Java/swap.java
+++ Java/swap.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class swap {
     public static void swapPairs(int arr[]) {
         for (int i = 0; i < arr.length - 1; i += 2) {
-
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        input.close();
     }
     
     
