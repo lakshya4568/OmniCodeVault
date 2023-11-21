32c823bc93a5fd6eaf5f8ba5f94d79c90076abd9
Lakshya Sharma <lakshya4568@gmail.com>
2023-11-21 13:30:01 +0530

    wrote the driver code of the swapPairs

--- Java/swap.java
+++ Java/swap.java
@@ -11,6 +11,19 @@ public class swap {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the array : ");
+        int size = input.nextInt();
+
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.printf("Enter %d element of the array : ", i);
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Before swapping: " + java.util.Arrays.toString(arr));
+        swapPairs(arr);
+        System.out.println("After swapping : " + java.util.Arrays.toString(arr));
 
         input.close();
     }
