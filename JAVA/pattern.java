93035fa990da55ae5a50e8c01169c9d39ff3c7bd
Lakshya Sharma <lakshya4568@gmail.com>
2023-12-28 01:29:57 +0530

    Add new pattern methods and update existing ones

--- JAVA/pattern.java
+++ JAVA/pattern.java
@@ -23,10 +23,8 @@ public class pattern {
         System.out.println();
     }
 
-
-
     public static void diamond(int lines) {
-        // space 
+        // space
         for (int i = 1; i <= lines; i++) {
             for (int j = 1; j <= lines - i; j++) {
                 System.out.print(" ");
@@ -62,7 +60,22 @@ public class pattern {
         }
         System.out.println();
     }
-    
+
+    public static void right_HalfPyramid(int lines) {
+        // outer loop
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= lines - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void numeric_half_pyramid(int lines) {
         for (int i = 1; i <= lines; i++) {
             for (int j = 1; j <= i; j++) {
@@ -70,8 +83,19 @@ public class pattern {
             }
             System.out.println();
         }
+        System.out.println();
     }
-    
+
+    public static void numeric_inverted_HalfPyramid(int lines) {
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= lines - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void inverted_HalfPyramid(int lines) {
         for (int i = 1; i <= lines; i++) {
             for (int j = 1; j <= lines - i + 1; j++) {
@@ -81,37 +105,132 @@ public class pattern {
         }
         System.out.println();
     }
-    
+
     public static void character_HalfPyramid(int lines) {
-        char ch = 'A'; 
+        char ch = 'A';
         for (int i = 1; i <= lines; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(ch);
                 ch++;
             }
             System.out.println();
-            /* If we want this type of pattern like :
-                A
-                AB                  We can change the inner loop by this algorithm
-                ABC                 for (char j = 1; j <= 'A' + i -1; j++) {
-                ABCD                    System.out.print(j); }
-                
-                when i = 2, the loop condition is j <= 'A' + i - 1. 
-                Here, i is 2, so the condition becomes j <= 'B'.
-            
-                In Java, characters are represented as integers based 
-                on their Unicode values. 'A' has a smaller Unicode value than 'B', 
-                so the condition 'A' <= 'B' is true.            
-            */
+            /*
+             * If we want this type of pattern like :
+             * A
+             * AB We can change the inner loop by this algorithm
+             * ABC for (char j = 1; j <= 'A' + i -1; j++) {
+             * ABCD System.out.print(j); }
+             * 
+             * when i = 2, the loop condition is j <= 'A' + i - 1.
+             * Here, i is 2, so the condition becomes j <= 'B'.
+             * 
+             * In Java, characters are represented as integers based
+             * on their Unicode values. 'A' has a smaller Unicode value than 'B',
+             * so the condition 'A' <= 'B' is true.
+             */
         }
+        System.out.println();
+    }
 
+    public static void floyds_triangle(int lines) {
+        int counter = 1;
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
+    public static void zero_one_triangle(int lines) {
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1" + " ");
+                } else {
+                    System.out.print('0' + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void butterfly(int lines) {
+        // 1st half
+        for (int i = 1; i <= lines; i++) {
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // space - 2(lines - i)
+            for (int j = 1; j <= 2 * (lines - i); j++) {
+                System.out.print(" ");
+            }
+            // star again - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half
+        for (int i = lines; i >= 1; i--) {
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // space - 2(lines - i)
+            for (int j = 1; j <= 2 * (lines - i); j++) {
+                System.out.print(" ");
+            }
+            // star again - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void solid_Rhombus(int lines) {
+        for (int i = 1; i <= lines; i++) {
+            // spaces - (n - i) 
+            for (int j = 1; j <= (lines - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= lines; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
     
+    public static void hollow_rhombus(int lines) {
+        for (int i = 1; i <= lines; i++) {
+            // spaces
+            for (int j = 1; j <= (lines - i); j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= lines; j++) {
+                if (i == 1 || j == 1 || j == lines || i == lines) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println();
-        
+
         System.out.print("Enter no. of rows : ");
         int rows = input.nextInt();
 
@@ -123,12 +242,17 @@ public class pattern {
         hollow_rectangle(rows, columns);
         diamond(rows);
         half_pyramid(rows);
-        SolidSquare(rows); 
+        SolidSquare(rows);
         inverted_HalfPyramid(rows);
         numeric_half_pyramid(rows);
-
-        System.out.println();
         character_HalfPyramid(rows);
+        right_HalfPyramid(rows);
+        numeric_inverted_HalfPyramid(columns);
+        floyds_triangle(columns);
+        zero_one_triangle(rows);
+        butterfly(rows);
+        solid_Rhombus(columns);
+        hollow_rhombus(rows);
 
         input.close();
     }
