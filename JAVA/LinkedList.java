9d19dc2e1fc9b341eb741c2346d18991223c51fb
Lakshya Sharma <lakshya4568@gmail.com>
2024-06-15 01:02:34 +0530

    wrote the Linkedlist code in java

--- JAVA/LinkedList.java
+++ JAVA/LinkedList.java
@@ -1,73 +1,186 @@
+import java.util.*;
+
 public class LinkedList {
-    class Node {
-        int data;
-        Node next;
+  class Node {
+    int data;
+    Node next;
 
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
+    public Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  // size of linked list (it will not traverse the linked list on each step)
+  public static int size;
+
+  public void addBegin(int data) {
+    Node temp = new Node(data);
+    size++;
+    // check if linked list is empty or not
+    if (head == null) {
+      head = tail = temp;
+      return;
+    } else {
+      temp.next = head;
+      head = temp;
+    }
+  }
+
+  public void addLast(int data) {
+    Node temp = new Node(data);
+    size++;
+    if (head == null) {
+      head = tail = temp;
+      return;
+    } else {
+      tail.next = temp;
+      tail = temp;
+    }
+  }
+
+  public void append(int data) { // append the data at end of list
+    Node temp = new Node(data);
+    size++;
+    if (head == null) {
+      head = tail = temp;
+      return;
     }
 
-    public static Node head;
-    public static Node tail;
-
-    public void addAtbegin(int data) {
-        // create a new node
-        Node temp = new Node(data);
-        if (head == null) {
-            head = tail = temp;
-            return;
-        } else {
-            temp.next = head;
-            head = temp;
-        }
+    Node current = head;
+    while (current.next != null) {
+      current = current.next;
     }
+    current.next = temp;
+    tail = temp;
+  }
 
-    public void addAtlast(int data) {
-        //create a new node 
-        Node temp = new Node(data);
-        if (head == null) {
-            head = tail = temp;
-            return;
-        } else {
-            tail.next = temp;
-            tail = temp;
-        }
+  public void add(int index, int data) { // at any index and location
+    // Linked list already exist
+    if (index < 0 || index > size) {
+      System.out.println("Invalid index: " + index);
+      return;
     }
-
-    public void append(int data) {
-        Node temp = new Node(data);
-        if (head == null) {
-            head = tail = temp;
-            return;
-        }
-        Node current = head;
-        while (current.next != null) {
-            current = current.next;
-        }
-        current.next = temp;
+    if (index == 0) {
+      addBegin(data);
+      return;
     }
- 
-
-    public void display() {
-        Node p = head;
-        while (p != null) {
-            System.out.printf("[%d]-->", p.data);
-            p = p.next;
-        }
+    if (index == size) {
+      addLast(data);
+      return;
     }
-
-    public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-
-        
-        ll.addAtlast(3);
-        ll.addAtlast(4);
-        ll.addAtlast(5);
-        ll.addAtbegin(1);
-        ll.addAtbegin(2);
-
-        ll.display();
+    Node new_Node = new Node(data);
+    size++;
+    Node temp = head;
+    int i = 0;
+    while (i < index - 1) {
+      temp = temp.next;
+      i++;
     }
+    new_Node.next = temp.next;
+    temp.next = new_Node;
+  }
+
+  public int recursiveSearch(int data) {}
+
+  public int length() {
+    Node current = head;
+    int length = 0;
+    if (head == null) {
+      return 0;
+    }
+    while (current != null) {
+      current = current.next;
+      length++;
+    }
+    return length;
+  }
+
+  public int removeFirst() {
+    if (size == 0) {
+      System.out.println("Linked List is empty");
+      return Integer.MIN_VALUE;
+    } else if (size == 1) {
+      int val = head.data;
+      head = tail = null;
+      size = 0;
+      return val;
+    } else {
+      int val = head.data;
+      head = head.next;
+      size--;
+      return val;
+    }
+  }
+
+  public int removeLast() {
+    if (size == 0) {
+      System.out.println("Linked List is empty");
+      return Integer.MIN_VALUE;
+    } else if (size == 1) {
+      int val = head.data;
+      head = tail = null;
+      size = 0;
+      return val;
+    } else {
+      // tail previous -> i = size -2;
+      Node prev = head;
+      for (int i = 0; i < size - 2; i++) {
+        prev = prev.next;
+      }
+      int val = prev.next.data;
+      prev.next = null;
+      tail = prev;
+      size--;
+      return val;
+    }
+  }
+
+  public int iterativeSearch(int key) {
+    Node temp = head;
+    int i = 0;
+    while (temp.next != null) {
+      if (temp.data == key) {
+        return i;
+      }
+      temp = temp.next;
+      i++;
+    }
+    return -1;
+  }
+
+  public void display() {
+    Node tempNode = head;
+    if (head == null) {
+      System.out.println("Linked List is empty");
+      return;
+    }
+    while (tempNode != null) {
+      System.out.printf("[%d]->", tempNode.data);
+      tempNode = tempNode.next;
+    }
+    System.out.println("NULL");
+  }
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    // create the Node cobject
+    LinkedList ll = new LinkedList();
+
+    ll.add(0, 8);
+    ll.append(5);
+    ll.append(9);
+    ll.display();
+    ll.removeFirst();
+    ll.display();
+
+    System.out.println(ll.iterativeSearch(9));
+
+    input.close();
+  }
 }
+
--- JAVA/dynamic.java
+++ JAVA/dynamic.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class dynamic {
@@ -22,20 +23,43 @@ public class dynamic {
         for (int i = 2; i <= n; i++) {
             f[i] = f[i - 1] + f[i - 2];
         }
-        
+
         return f[n];
+    }
+    
+    public static int countWays(int n, int ways[]) {
+        // through memoization
+        // base case
+        if (n == 0) {
+            // we have one way
+            return 1;
+        }
+        if (n < 0) {
+            // when n is negative -> -1, we have 0 ways
+            return 0;
+        }
+
+        if (ways[n] != -1) {
+            // we store the calculated value of the given ways
+            return ways[n];
+        }
+
+        ways[n] = countWays(n - 1, ways) + countWays(n - 2, ways);
+        return ways[n];
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
+
         System.out.print("Enter the input value : ");
         int n = input.nextInt();
-        long f[] = new long[n + 1];
-        
-        System.out.println(fib(n, f));
-        System.out.println(fiboTabulation(n));
+        //long f[] = new long[n + 1];
 
+        int ways[] = new int[n + 1];
+        Arrays.fill(ways, -1);
+        
+        System.out.println(countWays(n, ways));
         input.close();
     }
 }
