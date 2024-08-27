0b82e2ab7be4b235c452733613a36da4b4e37412
Lakshya Sharma <lakshya4568@gmail.com>
2024-08-27 12:18:47 +0530

    feat: Add OOPS class with Pen, Student, and BankAccount inner classes
    getting started with oops

--- JAVA/OOPS.java
+++ JAVA/OOPS.java
@@ -8,3 +8,46 @@
 7. access modifiers: private, public, default
 8. static and final
 */
+
+// classes are blueprint and object are the entities based on the class
+
+public class OOPS {
+    
+    class Pen { // by convention, the classes names start with first letter
+        String color; // attributes(properties)
+        int tip; // data memebers 
+
+        void setColor(String newColor) {
+            color = newColor;
+        }
+
+        void setTip(int newTip) {
+            tip = newTip;
+        }
+    }
+    
+    class Student {
+        String name;
+        int age;
+        float percentage;
+
+        void calcPercentage(int phy, int chem, int maths) {
+            percentage = (phy + chem + maths) / 3;
+        }
+    }
+
+    class BankAccount {
+        public String username;
+        private String password;
+
+    }
+
+    public static void main(String[] args) { // main method, compiler looks for main, and nothing else
+        // its where the function starts executing
+
+        OOPS outer = new OOPS();
+        BankAccount myAccount = outer.new BankAccount();
+        myAccount.username = "lakshyaSharma";
+        myAccount.password = "abced";
+    }
+}
