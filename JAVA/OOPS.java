75534bcb56ce29c10b1d138d63392c770438575c
Lakshya Sharma <lakshya4568@gmail.com>
2024-09-13 15:37:48 +0530

    Included the single level inheritance concept throug animal and fish class

--- JAVA/OOPS.java
+++ JAVA/OOPS.java
@@ -24,10 +24,19 @@ public class OOPS {
         Pen p1 = new Pen();
         p1.setColor("Blue");
         System.out.println(p1.color);
+        p1.model[0] = 21;
+        p1.model[1] = 52;
 
-        Pen p2 = new Pen();
-        System.out.println();
+        Pen p2 = new Pen(p1);
+        p1.model[0] = 3;
+        p1.model[1] = 34;
+        for (int i = 0; i < 2; i++) {
+            System.out.println(p2.model[i]);
+        }
 
+        Fish shark = new Fish();
+        shark.name = "Whale Shark";
+        shark.eat();
     }
 }
 
@@ -40,6 +49,23 @@ class BankAccount {
     }
 }
 
+class Animal {
+    String name;
+    String color;
+
+    void eat() {
+        System.out.println(this.name + " Eats");
+    }
+
+    void breath() {
+        System.out.println(this.name + "breathe");
+    }
+}
+
+class Fish extends Animal {
+    int fins; // count of fins
+}
+
 class Pen {
     String color;
     int tip;
@@ -49,6 +75,7 @@ class Pen {
     // non parametrerized constructor
     Pen() {
         System.out.println("Pen constructor is called");
+        model = new int[2];
     }
 
     // copy constructor -> not default by java
@@ -57,11 +84,27 @@ class Pen {
         getting copied, after copy constructor called even then the values will also get
         copied for the current object
     */
-    Pen(Pen p1) {
-        this.color = p1.color;
-        this.tip = p1.tip;
-        this.penCode = p1.penCode;
+    
+    // deep copy constructor, whole object is created new
+    Pen(Pen p) {
+        model = new int[2];
+        this.color = p.color;
+        this.tip = p.tip;
+        for (int i = 0; i < model.length; i++) {
+            this.model[i] = p.model[i]; // each element get copied in new object     
+        } 
     }
+
+    // shallow copy constructor
+    // only refrences get copied here
+
+    // Pen(Pen p1) {
+    //     model = new int[2];
+    //     this.color = p1.color;
+    //     this.tip = p1.tip;
+    //     this.penCode = p1.penCode;
+    //     this.model = p1.model;
+    // }
 
     // getters - return the value of object property
     String getColor() {
